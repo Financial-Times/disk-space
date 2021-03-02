529cf245affd5d894dcc5bac272f8ab80be9610f
Jake Champion <me@jakechampion.name>
2021-03-02 11:29:35 +0000

    Add JSDoc for the package

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-var cp       = require('child_process');
+var cp = require('child_process');
 
-module.exports  = function(mountedon , chain){
+/**
+ * @callback diskspaceCallback
+ * @param {Error|null} error Will be an error if the call to `df` fails
+ * @param {{usedSize: Number, totalSize: Number}} [result] An object where the property `usedSize` is the amount of kilobytes used and the property `totalSize` is the amount of kilobytes in total (used + available)
+ * @return {void}
+ */
+
+/**
+ * Find the used and total size of the file system that a given file is stored on in kilobytes
+ *
+ * @param {String} path The path to file whose file system you want to find the used and total size of
+ * @param {diskspaceCallback} callback A NodeJS Error-first style callback which is called when the used and total size have been calculated
+ */
+module.exports  = function(path , callback){
 
     var dfArgs = process.platform === 'darwin' ? '-bk' : "-BK";
-    var ps   = cp.spawn("df", [dfArgs , mountedon]);
+    var ps   = cp.spawn("df", [dfArgs , path]);
     var _ret = "";
 
     ps.stdout.on("data", function(data){
@@ -11,17 +24,18 @@ module.exports  = function(mountedon , chain){
     });
 
     ps.on('error' , function(err){
-      chain(err)
+      callback(err)
     });
 
     ps.on('close', function() {
-        var storageDeviceInfo ;
+        var storageDeviceInfo = { usedSize: 0, totalSize: 0};
         if(_ret.split('\n')[1]){
           var arr = _ret.split('\n')[1].split(/[\s,]+/);
-          storageDeviceInfo = {};
-          storageDeviceInfo.usedSize  = parseInt(arr[2].replace("K" , "")) * 1024;    // exp "300K" => 300
-          storageDeviceInfo.totalSize = parseInt(arr[3].replace("K" , ""))  * 1024 + storageDeviceInfo.usedSize;
+          var usedSize  = parseInt(arr[2].replace("K" , "")) * 1024;    // exp "300K" => 300
+          var totalSize = parseInt(arr[3].replace("K" , ""))  * 1024 + usedSize;
+          storageDeviceInfo.usedSize = usedSize;
+          storageDeviceInfo.totalSize = totalSize;
         }
-        chain(null , storageDeviceInfo);
+        callback(null, storageDeviceInfo);
     });
 };
